b05c0008cf4f4240c197dc395383575ee97fb860
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-12-01 08:25:09 +0100

    Add files via upload

---
+++ src/main/java/com/nttdata/practicaslogs/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.nttdata.practicaslogs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@RestController
+public class TestController {
+	
+	private Counter counter;
+	
+	public TestController(MeterRegistry registry) {
+		this.counter = Counter.builder("Invocaciones.hello").description("Invocaciones totales").register(registry);
+	}
+	
+	@GetMapping(path="/helloWorld")
+	public String helloWorld() {
+		counter.increment();
+		return "Hello World";
+	}
+
+}
